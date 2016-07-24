6b7753bda2cdf2a19722a6c8e64e201f3f54fad2
scav <dagherad@gmail.com>
2016-07-24 23:26:01 +0200

    moved prefix to its own definition

--- src/main/java/com/devbugger/pagery/transform/pagery/PageryMarkers.java
+++ src/main/java/com/devbugger/pagery/transform/pagery/PageryMarkers.java
@@ -10,42 +10,47 @@ package com.devbugger.pagery.transform.pagery;
 public class PageryMarkers {
 
     /**
+     * Defines the prefix used to define a marker.
+     */
+    private static final String PREFIX = "@";
+
+    /**
      * Pagery base markers for {@link com.devbugger.pagery.site.BasePage}.
      * These are used when generating the basic template all other will be
      * extended from.
      */
-    public static final String PAGERY_PAGES = "@pagery.pages";
-    public static final String PAGERY_TITLE = "@pagery.title";
-    public static final String PAGERY_CONTENT = "@pagery.content";
-    public static final String PAGERY_SITE_INFO = "@pagery.info";
-    public static final String PAGERY_BUILD_INFO = "@pagery.buildinfo";
+    public static final String PAGERY_PAGES = PREFIX+"pagery.pages";
+    public static final String PAGERY_TITLE = PREFIX+"pagery.title";
+    public static final String PAGERY_CONTENT = PREFIX+"pagery.content";
+    public static final String PAGERY_SITE_INFO = PREFIX+"pagery.info";
+    public static final String PAGERY_BUILD_INFO = PREFIX+"pagery.buildinfo";
 
     /**
      * Post wrappers.
      * These wrap the post data and provides a way to define
      * where in the template the data is extracted.
      */
-    public static final String POST_ALL = "@post.all";
-    public static final String POST_END = "@post.end";
+    public static final String POST_ALL = PREFIX+"post.all";
+    public static final String POST_END = PREFIX+"post.end";
 
     /**
      * {@link com.devbugger.pagery.site.Post} data for {@link TransformPagery} where to extract
      * data at matching markers.
      */
-    public static final String POST_TITLE = "@post.title";
-    public static final String POST_AUTHOR = "@post.author";
-    public static final String POST_DATE = "@post.date";
-    public static final String POST_CATEGORIES = "@post.categories";
-    public static final String POST_LEAD_PARAGRAPH = "@post.lead";
-    public static final String POST_LEAD_PARAGRAPH_START = "@post.lead.start";
-    public static final String POST_LEAD_PARAGRAPH_END = "@post.lead.end";
+    public static final String POST_TITLE = PREFIX+"post.title";
+    public static final String POST_AUTHOR = PREFIX+"post.author";
+    public static final String POST_DATE = PREFIX+"post.date";
+    public static final String POST_CATEGORIES = PREFIX+"post.categories";
+    public static final String POST_LEAD_PARAGRAPH = PREFIX+"post.lead";
+    public static final String POST_LEAD_PARAGRAPH_START = PREFIX+"post.lead.start";
+    public static final String POST_LEAD_PARAGRAPH_END = PREFIX+"post.lead.end";
 
     /**
      * {@link com.devbugger.pagery.site.Page} data for {@link TransformPagery} where to extract
      * data of site content.
      */
-    public static final String PAGE_TITLE = "@page.title";
-    public static final String PAGE_DATE = "@page.date";
+    public static final String PAGE_TITLE = PREFIX+"page.title";
+    public static final String PAGE_DATE = PREFIX+"page.date";
 
 
 
