5fb0b7ed933876ebe9e78bdf4210b641e4d900c3
scav <dagherad@gmail.com>
2016-07-24 23:35:55 +0200

    added simple comment to configsupport interface

--- src/main/java/com/devbugger/pagery/configuration/PageryConfigSupport.java
+++ src/main/java/com/devbugger/pagery/configuration/PageryConfigSupport.java
@@ -1,5 +1,14 @@
 package com.devbugger.pagery.configuration;
 
+/**
+ * Attach a {@link Config} instance.
+ */
 public interface PageryConfigSupport {
+
+    /**
+     * In cases where a class needs access to the {@link Config} object,
+     * this method is useful to provide said access.
+     * @param config current config object
+     */
     void setConfig(Config config);
 }
