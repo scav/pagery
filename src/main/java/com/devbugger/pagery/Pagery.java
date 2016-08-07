3b93bd8e175305d1cd70b60054dce4092323d0ae
scav <dagherad@gmail.com>
2016-08-07 23:01:07 +0200

    temp commit

--- src/main/java/com/devbugger/pagery/Pagery.java
+++ src/main/java/com/devbugger/pagery/Pagery.java
@@ -2,11 +2,26 @@ package com.devbugger.pagery;
 
 public class Pagery {
 
+    private static String SCAFFOLD = "scaffold";
+    private static String SERVER = "server";
+
     public static void main(String[] args) {
         if(args.length == 0) {
             PageryDefault pageryDefault = new PageryDefault();
             pageryDefault.generate();
             pageryDefault.server();
+        }
+        else {
+            for (String arg : args) {
+                if(arg.equals(SCAFFOLD)) {
+                    System.out.println("Starting scaffold");
+                    //scaffolding code here.
+                }
+                else if(arg.equals(SERVER)) {
+                    PageryDefault pageryDefault = new PageryDefault();
+                    pageryDefault.server();
+                }
+            }
         }
     }
 }
